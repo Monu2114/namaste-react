04e50f434e228ddc6b86483b0fd0f607a124ce5a
Monu_2114 <monu.nanabala@gmail.com>
2025-08-31 20:16:05 +0530

    day 2 - learnt about parcel , bundlers , create-react-app underhood , npm , npx , package.json and lock

---
+++ day-2/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const h1 = React.createElement("h1", {}, "Hi I am h1");
+const children = React.createElement("div", { id: "child" }, h1);
+const parent = React.createElement("div", { id: "parent" }, children);
+
+function Time({ name, bored }) {
+  return React.createElement("h1", {}, "Good", name, bored);
+}
+const element = React.createElement(Time, { name: "afternoon" });
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
