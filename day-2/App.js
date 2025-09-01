57fc693aebc8e1963764b35fcdf6b4a55ebc87af
Monu_2114 <monu.nanabala@gmail.com>
2025-09-01 13:50:02 +0530

    jsx , react component , {}

--- day-2/App.js
+++ day-2/App.js
@@ -1,13 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import koala from "./images/koala.jpg";
 
 const h1 = React.createElement("h1", {}, "Hi I am h1");
-const children = React.createElement("div", { id: "child" }, h1);
-const parent = React.createElement("div", { id: "parent" }, children);
-
-function Time({ name, bored }) {
-  return React.createElement("h1", {}, "Good", name, bored);
-}
-const element = React.createElement(Time, { name: "afternoon" });
+console.log(h1);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+console.log(root);
+
+// using jsx
+const jsxheading = <h1>Hii jsx bla bla</h1>;
+root.render(jsxheading);
+
+const ReactComponent = () => {
+  return <h2>React component</h2>;
+};
+console.log(ReactComponent());
+console.log(<ReactComponent />);
+root.render(ReactComponent());
+
+// react component
+// class based component - old
+// functional component - new
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={koala} alt="koala" width="200" />
+      {console.log("koala import:", koala)}
+
+      <input type="text" />
+      <img src={koala} alt="" />
+    </div>
+  );
+};
+root.render(<Header />);
